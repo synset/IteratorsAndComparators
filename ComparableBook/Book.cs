e9ef2f5d00adbf9a4843b8f1464e0b1fe872ef3a
synset <synnove.settman@yahoo.com>
2020-05-26 15:46:33 +0200

    Add project files.

---
+++ ComparableBook/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ComparableBook
+{
+    public class Book :IComparable<Book>
+    {
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public IReadOnlyList<string> Authors { get; private set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+
+        public int CompareTo(Book other)
+        {
+            int result = Year.CompareTo(other.Year);
+            if (result == 0)
+            {
+                result = Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
---
+++ LibraryIterator/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryIterator
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+    }
+}
